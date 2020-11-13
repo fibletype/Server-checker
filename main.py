57b35b77149cf510d7a949096958a6811b7a3934
fibletype <fibletyp@gmail.com>
2020-11-13 14:31:31 +0300

    add check_server function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
+import logging
+import requests
 
+from time import sleep
 from twilio.rest import Client
 from dotenv import load_dotenv
+from requests.exceptions import HTTPError
 
+logging.basicConfig(level=logging.INFO)
 
 def send_sms(text):
 
@@ -11,12 +16,26 @@ def send_sms(text):
     TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
     TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    try:
+        message = client.messages.create(
+            body = text,  
+            from_ = os.getenv('FROM_NUMBER'),  
+            to = os.getenv('TO_NUMBER'),  
+            )
+        logging.info('Message sent')
+    except Exception:
+        logging.error('Message is not sent')
 
-    message = client.messages.create(
-        body = text,  
-        from_ = '+12055129864',  
-        to = '+79819622766',  
-        )
+def check_server(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError:
+        send_sms('Site unavalibale')
+
 
 if __name__ == "__main__":
-    send_sms("Hello, Fibletype")
+    status = True
+    while True:
+        check_server('https://python101.online')
+        sleep(120)
